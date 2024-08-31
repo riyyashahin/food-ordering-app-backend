025f31a2cb2f7527c074992ab349b650788c9d99
Nihadh Muneer <nihadhmuneer@Nihadhs-MacBook-Air.local>
2024-08-31 13:16:37 +0400

    adding checkout flow

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import myUserRoutes from "./routes/MyUserRoute";
 import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -18,7 +19,7 @@ cloudinary.config({
 });
 
 const app = express();
-app.use(express.json());
+
 //automatically convert the body to any request we make from our API server to json
 app.use(cors());
 //going to add a test endpoint
@@ -30,12 +31,16 @@ app.use(cors());
 // the below will tell express that any request that start with /api/my/user
 // is going to forward the request onto myUserRoute file
 
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+app.use(express.json());
+
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "health OK!" });
 });
 app.use("/api/my/user", myUserRoutes);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -4,6 +4,16 @@ import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router();
 
+router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("RestaurantId parameter must be a valid string"),
+  RestaurantController.getRestaurant
+);
+
 // /api/restaurant/search/london
 router.get(
   "/search/:city",
@@ -12,7 +22,7 @@ router.get(
     .trim()
     .notEmpty()
     .withMessage("City parameter must be a valid string"),
-    RestaurantController.searchRestaurant
+  RestaurantController.searchRestaurant
 );
 
 export default router;
